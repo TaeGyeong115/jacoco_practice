fdd5ac8f85a91454650587c4c9c33c51f1b63020
TaeGyeong115 <115taegyeong@gmail.com>
2024-02-03 19:55:22 +0900

    init

---
+++ src/main/java/com/jacoco_practice/JacocoPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jacoco_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacocoPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jacoco_practice/JacocoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacoco_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
